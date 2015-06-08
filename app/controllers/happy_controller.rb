42fffd41f84043624dd1f4f0735271d24e304968
Joseph Ku <jku@duettoresearch.com>
2015-06-07 22:25:01 -0700

    Add AJAX implementation to Happy app.

--- app/controllers/happy_controller.rb
+++ app/controllers/happy_controller.rb
@@ -5,9 +5,11 @@ class HappyController < ApplicationController
 
   def joke
     @joke = Joke.offset(rand(Joke.count)).first
+    render 'joke', layout: false
   end
 
   def quote
     @quote = Quote.offset(rand(Quote.count)).first
+    render 'quote', layout: false
   end
 end
